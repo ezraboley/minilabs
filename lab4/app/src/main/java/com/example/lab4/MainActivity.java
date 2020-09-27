d797e339801f03020dfeccf78b870894883058fb
Ezra Boley <eboley@wisc.edu>
2020-09-27 14:26:52 -0500

    Task 1 complete

---
+++ lab4/app/src/main/java/com/example/lab4/MainActivity.java
@@ -0,0 +1,146 @@
+
+//
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
+
+package com.example.lab4;
+
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.lab4.R;
+
+public class MainActivity extends Activity implements SensorEventListener {
+    private SensorManager sensorManager;
+    private boolean color = false;
+    private View view;
+    private long lastUpdate;
+    TextView textx, texty, textz;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        // get textviews
+        textx = (TextView) findViewById(R.id.xval);
+        texty = (TextView) findViewById(R.id.yval);
+        textz = (TextView) findViewById(R.id.zval);
+
+
+        view = findViewById(R.id.textView);
+        view.setBackgroundColor(Color.BLUE);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+        lastUpdate = System.currentTimeMillis();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            displayAccelerometer(event);
+            checkShake(event);
+        }
+
+    }
+
+    private void displayAccelerometer(SensorEvent event) {
+
+        // Accelerometer sensor returns 3 values, one for each axis.
+        // Your code here:
+        final float x = event.values[0];
+        final float y = event.values[1];
+        final float z = event.values[2];
+
+        // display values using TextView
+        // Your code here:
+        view.post(new Runnable() {
+            @Override
+            public void run() {
+                textx.setText(String.format("X: %f", x));
+                texty.setText(String.format("Y: %f", y));
+                textz.setText(String.format("Z: %f", z));
+            }
+        });
+
+
+    }
+
+    private void checkShake(SensorEvent event) {
+
+        // Movement
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+
+        float accelerationSquareRoot = (x * x + y * y + z * z)
+                / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+        long actualTime = System.currentTimeMillis();
+        if (accelerationSquareRoot >= 2) //
+        {
+            if (actualTime - lastUpdate < 200) {
+                return;
+            }
+            lastUpdate = actualTime;
+            Toast.makeText(this, "Don't shake me!", Toast.LENGTH_SHORT).show();
+            if (color) {
+                view.setBackgroundColor(Color.BLUE);
+
+            } else {
+                view.setBackgroundColor(Color.RED);
+            }
+            color = !color;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // register this class as a listener for the orientation and
+        // accelerometer sensors
+        sensorManager.registerListener(this,
+                sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // unregister listener
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+}
